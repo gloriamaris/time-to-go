4781685c5f2151b7c2e62919012cf6f0b4255458
Monique Dingding <monique.dingding@lamudi.com>
2019-08-17 21:27:46 +0800

    Integrate bulma as design component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './scss/index.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            Hello World
+            </h1>
+          <p className="subtitle">
+            My first website with <strong>Bulma</strong>!
+            </p>
+        </div>
+      </section>
+      <section className="section">
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="Input" />
+          </div>
+        </div>
+
+        <div className="field">
+          <p className="control">
+            <span className="select">
+              <select>
+                <option>Select dropdown</option>
+              </select>
+            </span>
+          </p>
+        </div>
+
+        <div className="buttons">
+          <a href="#!" className="button is-primary">Primary</a>
+          <a href="#!" className="button is-link">Link</a>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
